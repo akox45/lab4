6c98267998522ff38d2b7daad892d648207b0dc5
Akos Orosz <lekvar4thewin@gmail.com>
2025-03-19 19:01:37 -0400

    xd

--- photoalbum/photoalbum/settings.py
+++ photoalbum/photoalbum/settings.py
@@ -83,10 +83,10 @@ DATABASES = {
     }
 }
 
-import os
-if os.environ.get('DATABASE_URL'):
-    import dj_database_url
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# import os
+# if os.environ.get('DATABASE_URL'):
+#    import dj_database_url
+ #    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 
